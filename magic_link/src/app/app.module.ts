d2047cc6e049279d21d57eaf8b7e770769939172
scavascava <n.skawinska03@gmail.com>
2020-11-13 21:47:28 +0100

    authentication through magic link

---
+++ magic_link/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
+
+import { PasswordlessAuthComponent } from './passwordless-auth/passwordless-auth.component';
+
+const firebaseConfig = {
+  // paste Firebase Config HERE!
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PasswordlessAuthComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ magic_link/src/app/passwordless-auth/passwordless-auth.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-passwordless-auth',
+  templateUrl: './passwordless-auth.component.html',
+  styleUrls: ['./passwordless-auth.component.css']
+})
+export class PasswordlessAuthComponent implements OnInit {
+
+  user: Observable<any>;
+  email: string;
+  emailSent = false;
+  errorMessage: string;
+  
+
+  constructor(public afAuth: AngularFireAuth, private router: Router) { }
+
+  ngOnInit(): void {
+    this.user = this.afAuth.authState;
+    const url = this.router.url;
+    this.confirmSignIn(url);
+  }
+
+  async sendEmailLink(email: string) {
+    const actionCodeSettings = {
+      url: 'http://localhost:4200/login',
+      handleCodeInApp: true
+    };
+
+    try {
+      await this.afAuth.auth.sendSignInLinkToEmail(
+        this.email,
+        actionCodeSettings
+      );
+
+      window.localStorage.setItem('emailForSignIn', this.email);
+      this.emailSent = true;
+
+    } catch (err) {
+      this.errorMessage = err.message;
+    }
+  }
+
+  async confirmSignIn(url) {
+    try {
+      if (this.afAuth.auth.isSignInWithEmailLink(url)) {
+        let email = window.localStorage.getItem('emailForSignIn');
+
+        // If missing email, prompt user for it
+        if (!email) {
+          email = window.prompt('Please provide your email for confirmation')
+        }
+
+        // Signin user and remove he email LocalStorage
+        const result = await this.afAuth.auth.signInWithEmailLink(email, url);
+        window.localStorage.removeItem('emailForSignIn');
+      }
+    } catch (err) {
+      this.errorMessage = err.message;
+    }
+  }
+
+}
